6a048c3d7f0b895de895caaded956026acc0bbc1
eruditiocz@gmail.com <tomas.marek525@gmail.com>
2024-05-25 00:56:49 +0200

    Update webu
    Update webu

--- js/script.js
+++ js/script.js
@@ -25,5 +25,5 @@ const sr = ScrollReveal ({
     reset: true
 })
 
-sr.reveal(".home-text", {delay:190, origin:"bottom"})
+sr.reveal(".home-text, .home-image", {delay:190, origin:"bottom"})
 sr.reveal(".about,.services,.portfolio,.contact", {delay:200, origin:"bottom"})
